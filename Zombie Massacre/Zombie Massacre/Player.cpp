2c3b12bcab621c7fae1593a4c61b8d7383dba8d5
Aaron Hamilton <aaronleon28@gmail.com>
2021-10-04 15:08:30 +0100

    Housekeeping
    Spell checked the comments and gave the code a bit of reformatting and added regions where necessary

--- Zombie Massacre/Zombie Massacre/Player.cpp
+++ Zombie Massacre/Zombie Massacre/Player.cpp
@@ -3,16 +3,20 @@
 Player::Player()
 {
 	#pragma region Linking Variables
-	/*Linking the main constents to the variables*/
+
+	/*Linking the main contents to the variables*/
 	m_speed = START_SPEED;
 	m_health = START_HEALTH;
 	m_maxHealth = START_HEALTH;
+
 	#pragma endregion
 
-	#pragma region Linking Textures4
+	#pragma region Linking Textures
+
 	/*Setting image to texture and linking texture*/
 	m_texture.loadFromFile("Assets/graphics/player.png");
 	m_Sprite.setTexture(m_texture);
+
 	#pragma endregion
 
 	/*Getting the location*/
@@ -22,12 +26,15 @@ Player::Player()
 void Player::spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize)
 {
 	#pragma region Spawn Player Location
-	/*Spawn Player In center of PlaySpace*/
+
+	/*Spawn Player In centre of Play Space*/
 	m_position.x = arena.width / 2;
 	m_position.y = arena.height / 2;
+
 	#pragma endregion
 
 	#pragma region Sizing of area, Tiles, Resolution
+		
 	/*Load the arena to m_arena*/
 	m_arena.left = arena.left;
 	m_arena.width = arena.width;
@@ -40,13 +47,16 @@ void Player::spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize)
 	/*Save resolution*/
 	m_screenRes.x = resolution.x;
 	m_screenRes.y = resolution.y;
+
 	#pragma endregion
 }
 
 bool Player::hit(sf::Time timeHit)
 {
-	/*Creating a cool down hit time so player doesnt lose all their health at once*/
-	if (timeHit.asMicroseconds() - m_lastHit.asMilliseconds() > 200) 
+	#pragma region Cool Down
+
+	/*Creating a cool down hit time so player doesn’t lose all their health at once*/
+	if (timeHit.asMicroseconds() - m_lastHit.asMilliseconds() > 200)
 	{
 		/*Update last hit time to create the cool down then take 10 health*/
 		m_lastHit = timeHit;
@@ -55,165 +65,111 @@ bool Player::hit(sf::Time timeHit)
 	}
 	else
 	{
-		/*If cooldown isnt complete, do nothing*/
+		/*If cooldown isn’t complete, do nothing*/
 		return false;
 	}
+
+	#pragma endregion
 }
 
-sf::Time Player::lastHitTime()
-{
-	return sf::Time();
-}
+sf::Time Player::lastHitTime() { return sf::Time(); }
 
-sf::FloatRect Player::getPosition()
-{
-	/*Getting the current position from the sprite*/
-	return m_Sprite.getLocalBounds();
-}
+#pragma region Get Players Data on location and rotation
+/*Getting the current position from the sprite*/
+sf::FloatRect Player::getPosition() { return m_Sprite.getLocalBounds(); }
 
-float Player::getRotation()
-{
-	/*Get the current rotation of this sprite*/
-	return m_Sprite.getRotation();
-}
+/*Get the current rotation of this sprite*/
+float Player::getRotation() { return m_Sprite.getRotation(); }
 
-sf::Sprite Player::getSprite()
-{
-	return m_Sprite;
-}
+sf::Sprite Player::getSprite() { return m_Sprite; }
 
-int Player::getHealth()
-{
-	return m_health;
-}
+int Player::getHealth() { return m_health; }
 
-sf::Vector2f Player::getCenter()
-{
-	/*Get the Position which will tell us the center*/
-	return m_position;
-}
+/*Get the Position which will tell us the centre*/
+sf::Vector2f Player::getCenter() { return m_position; }
+#pragma endregion
 
-void Player::moveLeft()
-{
-	m_leftArrowPressed = true;
-}
+#pragma region Movement
+#pragma region Button Pressed
+void Player::moveLeft() { m_leftArrowPressed = true; }
 
-void Player::moveRight()
-{
-	m_rightArrowPressed = true;
-}
+void Player::moveRight() { m_rightArrowPressed = true; }
 
-void Player::moveUp()
-{
-	m_upArrowPressed = true;
-}
+void Player::moveUp() { m_upArrowPressed = true; }
 
-void Player::moveDown()
-{
-	m_downArrowPressed = true;
-}
+void Player::moveDown() { m_downArrowPressed = true; }
+#pragma endregion
 
-void Player::stopLeft()
-{
-	m_leftArrowPressed = false;
-}
+#pragma region Button Releced
+void Player::stopLeft() { m_leftArrowPressed = false; }
 
-void Player::stopRight()
-{
-	m_rightArrowPressed = false;
-}
+void Player::stopRight() { m_rightArrowPressed = false; }
 
-void Player::stopUp()
-{
-	m_upArrowPressed = false;
-}
+void Player::stopUp() { m_upArrowPressed = false; }
 
-void Player::stopDown()
-{
-	m_downArrowPressed = false;
-}
+void Player::stopDown() { m_downArrowPressed = false; }
+#pragma endregion
+#pragma endregion
 
 void Player::update(float elapsedTime, sf::Vector2i mousePos)
 {
 	#pragma region Movement
 	/*If detect arrow presses and move it by the speed set*/
-	if (m_upArrowPressed) 
-	{
-		m_position.y -= m_speed * elapsedTime;
-	}
-	if (m_downArrowPressed)
-	{
-		m_position.y += m_speed * elapsedTime;
-	}
+	if (m_upArrowPressed){ m_position.y -= m_speed * elapsedTime; }
 
-	if (m_leftArrowPressed)
-	{
-		m_position.x -= m_speed * elapsedTime;
-	}
-	if (m_rightArrowPressed)
-	{
-		m_position.x += m_speed * elapsedTime;
-	}
+	if (m_downArrowPressed){ m_position.y += m_speed * elapsedTime;	}
+
+	if (m_leftArrowPressed){ m_position.x -= m_speed * elapsedTime; }
+
+	if (m_rightArrowPressed){ m_position.x += m_speed * elapsedTime; }
 	#pragma endregion
 
 	/*Update Position*/
 	m_Sprite.setPosition(m_position);
 
-	#pragma region Boundries
-	/*Making sure they cant leave the place space*/
-	if (m_position.x > m_arena.width - m_tilesize) {
-		m_position.x = m_arena.width - m_tilesize;
-	}
-	if (m_position.x > m_arena.left + m_tilesize) {
-		m_position.x = m_arena.left + m_tilesize;
-	}
+	#pragma region Boundaries
+	/*Making sure they can’t leave the play space*/
+	if (m_position.x > m_arena.width - m_tilesize){ m_position.x = m_arena.width - m_tilesize; }
 
-	if (m_position.y > m_arena.height - m_tilesize) {
-		m_position.y = m_arena.height - m_tilesize;
-	}
-	if (m_position.y > m_arena.top + m_tilesize) {
-		m_position.y = m_arena.top + m_tilesize;
-	}
+	if (m_position.x > m_arena.left + m_tilesize) { m_position.x = m_arena.left + m_tilesize; }
+
+	if (m_position.y > m_arena.height - m_tilesize) { m_position.y = m_arena.height - m_tilesize; }
+	
+	if (m_position.y > m_arena.top + m_tilesize) { m_position.y = m_arena.top + m_tilesize;	}
 	#pragma endregion
 
 	#pragma region Roation from Mouse
 	float angle = (atan2(mousePos.y - m_screenRes.y / 2,
-						mousePos.x - m_screenRes.x / 2) * 180)
-						/ 3.141;
+		mousePos.x - m_screenRes.x / 2) * 180)
+		/ 3.141;
 
 	m_Sprite.setRotation(angle);
 	#pragma endregion
 }
 
-void Player::upgradeHealth()
-{
-	/*Upgrade the health by 20%*/
-	m_maxHealth += (START_HEALTH * .2);
-}
+#pragma region Player Perks
+/*Upgrade the health by 20%*/
+void Player::upgradeHealth() { m_maxHealth += (START_HEALTH * .2); }
 
-void Player::upgradeSpeed()
-{
-	/*Upgrade the speed by 20%*/
-	m_speed += (START_SPEED * .2);
-}
+/*Upgrade the speed by 20%*/
+void Player::upgradeSpeed() { m_speed += (START_SPEED * .2); }
+
 
 void Player::increaseHealth(int amount)
 {
 	m_health += amount;
-	
+
 	/*But not pass the max health*/
-	if (m_health > m_maxHealth) {
-		m_health = m_maxHealth;
-	}
+	if (m_health > m_maxHealth) { m_health = m_maxHealth; }
 }
+#pragma endregion
 
 
 void Player::resetPlayerState()
 {
-	/*Setting varibles with the constent when game starts / resets*/
+	/*Setting variables with the constant when game starts / resets*/
 	m_speed = START_SPEED;
 	m_health = START_HEALTH;
 	m_maxHealth = m_maxHealth;
-
-
 }
+
